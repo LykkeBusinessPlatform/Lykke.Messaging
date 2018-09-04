98f387665867994d967a3af1e582b4fb9d08ef91
Dmitry Smolyakov <35897594+dmitrysmall@users.noreply.github.com>
2018-09-05 00:26:48 +0300

    Deserialization from binary message formats is now resilient. (#11)
    Deserialization from binary message formats is now resilient: we have 2 fallbacks, one from messagepack to protobuf, and another from protobuf to messagepack.

--- src/Lykke.Messaging/Serialization/MessagePackSerializer.cs
+++ src/Lykke.Messaging/Serialization/MessagePackSerializer.cs
@@ -1,62 +1,32 @@
-﻿using Common.Log;
+﻿using System;
+using Common.Log;
 using Lykke.Common.Log;
-using System;
 
 namespace Lykke.Messaging.Serialization
 {
     internal class MessagePackSerializer<TMessage> : IMessageSerializer<TMessage>
     {
-        private readonly ProtobufSerializer<TMessage> _protobufSerializer;
-        private readonly ILog _log;
+        private readonly ResilientBinarySerializer<TMessage> _serializer;
 
-        private bool _fallbackDeserializerWorks = false;
-
-        [Obsolete]
+        [Obsolete("Please, use the overload which consumes ILogFactory")]
         public MessagePackSerializer(ILog log)
         {
-            _protobufSerializer = new ProtobufSerializer<TMessage>();
-            _log = log ?? throw new ArgumentNullException(nameof(log));
+            _serializer = new ResilientBinarySerializer<TMessage>(log, SerializationFormat.MessagePack);
         }
 
         public MessagePackSerializer(ILogFactory logFactory)
         {
-            _protobufSerializer = new ProtobufSerializer<TMessage>();
-            _log = logFactory.CreateLog(this);
+            _serializer = new ResilientBinarySerializer<TMessage>(logFactory, SerializationFormat.MessagePack);
         }
 
         public byte[] Serialize(TMessage message)
         {
-            return MessagePack.MessagePackSerializer.Serialize(message, MessagePackSerializerFactory.Defaults.FormatterResolver);
+            return _serializer.Serialize(message);
         }
 
         public TMessage Deserialize(byte[] message)
         {
-            try
-            {
-                return MessagePack.MessagePackSerializer.Deserialize<TMessage>(message, MessagePackSerializerFactory.Defaults.FormatterResolver);
-            }
-            catch (Exception ex)
-            {
-                if (_fallbackDeserializerWorks)
-                {
-                    _log.WriteWarning(nameof(Deserialize), message, "MessagePack deserializer failed, using ProtoBuf");
-                }
-                else
-                {
-                    _log.WriteError(nameof(Deserialize), message, ex);
-                }
-                try
-                {
-                    var result = _protobufSerializer.Deserialize(message);
-                    if (!_fallbackDeserializerWorks)
-                        _fallbackDeserializerWorks = true;
-                    return result;
-                }
-                catch (Exception)
-                {
-                    throw ex;
-                }
-            }
+            return _serializer.Deserialize(message);
         }
     }
 }
