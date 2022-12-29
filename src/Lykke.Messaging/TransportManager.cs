0a0073b5f3fb154474d9066b9846d6a2cb39cdad
atarutin <andrey.tarutin@lykke-business.ch>
2022-12-29 16:47:13 +0300

    doc(LT-4278): minor comments while refactoring

--- src/Lykke.Messaging/TransportManager.cs
+++ src/Lykke.Messaging/TransportManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using Lykke.Messaging.Contract;
 using Lykke.Messaging.InMemory;
 using Lykke.Messaging.Transports;
